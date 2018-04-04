82d6c3e05290885542fae42edeae2b35844a4273
Glitch (fmm-feddle) <none>
2018-04-04 13:17:08 +0000

    1.0.0 commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
-
+const multer  = require('multer');
+const upload = multer({ limits: {fileSize: 2000000} });
 const app = express();
 
 
@@ -11,11 +12,19 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-
+app.post("/", upload.single("file"), (req, res) => {
+  const size = req.file.size;
+  res.send({size: size});
+})
 
 
 app.get("*", (req, res) => {
   res.status(404).end("Page not found");
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send(err)
 })
 
 
